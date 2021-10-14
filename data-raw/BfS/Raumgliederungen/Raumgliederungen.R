3781a006528cebb21e2e86c67e54e25612f1bc0b
Radek <r.panczak@gmail.com>
2021-10-14 18:52:45 +0200

    agglo_21

--- data-raw/BfS/Raumgliederungen/Raumgliederungen.R
+++ data-raw/BfS/Raumgliederungen/Raumgliederungen.R
@@ -1,3 +1,4 @@
+library(readr)
 library(readxl)
 library(dplyr)
 library(janitor)
@@ -13,7 +14,7 @@ raum_18 <- read_xlsx("data-raw/Raumgliederungen/2018-01-01_raumgliederungen.xlsx
 
 View(raum_18)
 
-saveRDS(raum_18, "data/Raumgliederungen/raum_18.Rds")
+write_rds(raum_18, "data/Raumgliederungen/raum_18.Rds")
 
 raum_20 <- read_xlsx("data-raw/Raumgliederungen/2020-01-01_raumgliederungen.xlsx", 
                      skip = 1, 
@@ -26,4 +27,16 @@ raum_20 <- read_xlsx("data-raw/Raumgliederungen/2020-01-01_raumgliederungen.xlsx
 
 View(raum_20)
 
-saveRDS(raum_20, "data/Raumgliederungen/raum_20.Rds")
+write_rds(raum_20, "data/Raumgliederungen/raum_20.Rds")
+
+agglo_21 <- read_excel("data-raw/Raumgliederungen/2021-04-18_agglo.xlsx", 
+                       col_types = c("numeric", "text", "numeric", 
+                                     "text", "numeric", "text", "numeric"),
+                       skip = 1) %>% 
+  clean_names() %>% 
+  remove_empty() %>% 
+  filter(!is.na(bfs_gde_nummer))
+
+View(agglo_21)
+
+write_rds(agglo_21, "data/Raumgliederungen/agglo_21.Rds")
